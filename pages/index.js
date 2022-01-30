8fc30188d5a9f1c835c23d05c862faeca9863571
alumonabenaiah@gmail.com <alumonabenaiah71@gmail.com>
2022-01-30 08:54:36 +0100

    updated collections and index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import { Error, Header, Months } from "../components";
 import NavBar from "../components/Navbar/Navbar";
 
+import style from "../styles/home.module.css";
+
 export default function Home() {
   return (
     <div>
       <Header />
-      <Error message="You have no collection" />
-      <Months />
+      {/* <Error message="You have no collection" /> */}
+      <div className={style.monthsCont}>
+        <Months />
+        <Months />
+        <Months />
+        <Months />
+      </div>
       <NavBar />
     </div>
   );
